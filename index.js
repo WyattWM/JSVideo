a5f83661032693b512794cb24ceaf4f213da20a1
wyattwm <wyattwmwm@gmail.com>
2024-02-19 12:02:23 -0700

    updated files

--- index.js
+++ index.js
@@ -944,7 +944,7 @@ console.log(myDog["dead"]);
 
 
 
-// OBJECTS FOR LOOKUPS //
+// OBJECTS FOR LOOKUPS // - you can use objects to look up values
 
 function phoneticLookup(val) {
     var result = ""
@@ -1407,33 +1407,233 @@ console.log(increment(5));
 
 // REST OPERATOR //
 
+const sum = (function() {
+    return function sum(x, y, z) {
+        const args = [ x, y, z ];
+        return args.reduce((a, b) => a + b, 0);
+    };
+})();
+console.log(sum(1, 2, 3));
+
 
 
 // SPREAD OPERATOR //
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+    arr2 = arr1;
+    arr1[0] = 'potato'
+})();
+console.log(arr2);
 
 
 
 // DESTRUCTING ASSIGNMENT //
 
+var voxel = {x: 3.6, y: 7.4, z: 6.54 };
+
+var x = voxel.x;
+var y = voxel.y;
+var z = voxel.z;
+
+const { x : a, y : b, z : c } = voxel;
+
+const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+    "use strict";
+
+    const getTempOfTmrw = undefined;
+
+    return getTempOfTmrw;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES));
+
+// Destructuring Assignment with Nested Objects //
+
+const LOCAL_FORECAST = {
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+    "use strict";
+
+    const getMaxOfTmrw = undefined;
+
+    return getMaxOfTmrw;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST));
+
+// Use Destructuring Assignment to Assign Variables from Arrays //
+
+const [z, x] = [1, 2, 3, 4, 5, 6];
+console.log(z, x);
+
+let a = 8, b = 6;
+(() => {
+    "use strict";
+
+})();
+
+// Use Destructuring Assignment with the Rest Operator //
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    const arr = list;
+
+    return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr);
+console.log(source);
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters //
+
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function() {
+    return function half(stats) {
+        return (stats.max + stats.min) / 2.0;
+    };
+
+})();
+console.log(stats);
+console.log(half(stats));
+
 
 
 // TEMPLATE LITERALS //
+
+const person = {
+    name: "Zodiac Hasbro",
+    age: 56
+};
+
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+    const resultDisplayArray = null;
+
+    return resultDisplayArray;
+}
+
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ * `<li class="text-warning">var-on-top</li>`,
+ * `<li class="text-warning">linebreak</li>`]
+ */
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
 
 
 
 // SIMPLE FIELDS //
 
+const createPerson = (name, age, gender) => {
+
+    return {
+        name: name,
+        age: age,
+        gender: gender
+    };
+
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male"));
+
 
 
 // DECLARATIVE FUNCTIONS //
+
+const bicycle = {
+    gear: 2,
+    setGear: function(newGear) {
+        "use strict";
+        this.gear = newGear;
+    }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
 
 
 
 // CLASS SYNTAX //
 
+var SpaceShuttle = function(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+}
+var zeus = new SpaceShuttle('Jupiter');
+
+console.log(zeus.targetPlanet);
+
+// function makeClass() {
+
+//  return Vegetable;
+// }
+// const Vegetable = makeCLass();
+// const carrot - new Vegetable('carrot');
+// console.log(carrot.name);
+
 
 
 // GETTERS AND SETTERS //
+
+class Book {
+    constructor(author) {
+        this._author = author;
+    }
+    // getter
+    get writer() {
+        return this._author;
+    }
+    // setter
+    set writer(updatedAuthor) {
+        this._author = updatedAuthor;
+    }
+}
+
+function makeClass() {
+    class Thermostat {
+        constructor(temp) {
+            this._temp = 5/9 * (temp - 32);
+        }
+        get temperature() {
+            return this._temp;
+        }
+        set temperature(updatedTemp) {
+            this._temp = updatedTemp;
+        }
+    }
+    return Thermostat;
+}
+
+const Thermostat = makeClass();
+const thermos = new Thermostat(76);
+let temp = thermos.temperature;
+thermos.temperature = 26;
+temp = thermos.temperature;
 
 
 
@@ -1441,63 +1641,7 @@ console.log(increment(5));
 
 
 
-//** CODECADEMY INTRO TO JAVASCRIPT VIDEO END **/
-
-
-
-
-//** JAVASCRIPT DOM MANIPULATION VIDEO **/
-// SELECTING ELEMENTS IN THE DOM //
-
-
-
-// STYLING AN ELEMENT //
-
-
-
-// CREATING ELEMENTS //
-
-
-
-// ADDING ELEMENTS //
-
-
-
-// MODIFY TEXT //
-
-
-
-// MODIFYING ELEMENTS ATTRIBUTES AND CLASSES //
-
-
-
-// REMOVE AN ELEMENT //
-
-
-
-// TRAVERSING THE DOM //
-
-
-
-// EVENT LISTENERS //
-
-
-
-// EVENT PROPAGATION //
-
-
-
-// EVENT DELEGATION //
-
-
-
-// PROJECTS //
-
-
-
-//** JAVASCRIPT DOM MANIPULATION VIDEO END **//
-
-
+//** CODECADEMY INTRO TO JAVASCRIPT VIDEO END **//
 
 
 //** ASYNCHRONUS JAVASCRIPT COURSE VIDEO START **//
